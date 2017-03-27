22bf8bd701352036820706786ad897acc8614759
yamnazaki_97 <f.ky@qq.com>
2017-03-27 09:52:22 +0800

    001-简介和开发准备工作

---
+++ src/main/java/com/wumugulu/Chapter01Application.java
@@ -0,0 +1,12 @@
+package com.wumugulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter01Application.class, args);
+	}
+}
